cec7e4c8e894e40ecf21c777dc28cfe8648f8e88
DefinitelyNotMigZ <103237073+DefinitelyNotMigZ@users.noreply.github.com>
2024-10-15 02:18:19 +0800

    hello

--- client/src/components/events/completeeventdialog.jsx
+++ client/src/components/events/completeeventdialog.jsx
@@ -17,7 +17,7 @@ const CompletedEventsDialog = ({ isOpen, onClose, completedEvents, onEventDelete
       alert('Failed to delete event. Please try again.');
     }
   };
-
+// hello
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
       <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
